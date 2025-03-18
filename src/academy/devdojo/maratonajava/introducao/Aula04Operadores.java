b813180597b081b776734995dc87d75dc077bce4
Augusto fragg <augustofragg@gmail.com>
2025-03-18 14:58:46 -0300

    Aula04 - operadores logico And

--- src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -60,6 +60,18 @@ public class Aula04Operadores {
         System.out.println("isDezDiferenteDez: " + isDezDiferenteDez);
 
 
+        //Operadores lógicos
+
+        // && (AND)  || (or)
+
+        int idade = 35;
+        float salario = 3500f;
+
+        boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+
+        System.out.println("isDentroDaLeiMaiorQueTrinta " + isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMennorQueTrinta " + isDentroDaLeiMenorQueTrinta);
 
     }
 }
