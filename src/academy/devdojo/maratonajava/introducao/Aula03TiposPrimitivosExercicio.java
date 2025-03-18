22794b47d68069f023645daa0f354b582564cbe8
Augusto fragg <augustofragg@gmail.com>
2025-03-18 12:52:08 -0300

    Aula03 - tipos primitivos - exercicios

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao;
+
+/*
+    Exercicio Pratico
+
+    Crie variáveis para os campos descritos abaixo entre <> e imprima a seguinte mensagem:
+
+    Eu <nome>, morando no <endereço>,
+    confirmo que recebi o salário de <salário>, na data <data>
+
+ */
+public class Aula03TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+
+        String name = "Carlos Augusto";
+        String address = "Avenida Alberto Algusto, numero 371";
+        String date = "18/03/2025";
+        double salary = 3000.00;
+
+        String relatorio = "Eu " + name + ", morando no " + address + "\nconfirmo que recebi o salario de " + salary + ", na data " + date ;
+
+        System.out.println(relatorio);
+
+
+    }
+}
