6970ca40f050fc0e23c6ea8551849117c2eb0a58
Augusto fragg <augustofragg@gmail.com>
2025-03-31 23:13:06 -0300

    Aula - OOP - exercicios associacao

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/AssociacaoTeste.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Aluno;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Local;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Professor;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Seminario;
+
+public class ExercicioTest {
+    public static void main(String[] args) {
+
+        
+    }
+}
