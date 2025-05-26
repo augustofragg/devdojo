5c14a39a18dfe3207a90937419463071c870e7d4
Augusto fragg <augustofragg@gmail.com>
2025-05-26 19:26:58 -0300

    Aula - Data Hora - part 2

--- src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Rdatas/test/PeriodTest01.java
@@ -1,4 +1,35 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 public class PeriodTest01 {
+    public static void main(String[] args) {
+
+        //Classe Period
+        //Trabalha com datas: Ano, meses, semanas e dias
+
+        LocalDate now = LocalDate.now();
+        LocalDate nowAfterTwoYears = LocalDate.now().plusYears(2);
+
+        Period p1 = Period.between(now,nowAfterTwoYears);
+        Period p2 = Period.ofDays(10);
+        Period p3 = Period.ofWeeks(58);
+        Period p4 = Period.ofMonths(3);
+        Period p5 = Period.ofYears(3);
+
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+        System.out.println("\n");
+
+        System.out.println(p3.getMonths());
+        System.out.println(Period.between(now,now.plusDays(p3.getDays())).getMonths());
+
+        System.out.println(now + "\n" + nowAfterTwoYears);
+
+
+    }
 }
