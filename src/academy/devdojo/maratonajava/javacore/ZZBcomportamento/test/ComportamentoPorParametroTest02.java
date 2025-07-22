c52b4f71ab390f5f0185aacbc4a34b1cc4c46859
Augusto fragg <augustofragg@gmail.com>
2025-07-22 14:15:04 -0300

    Aula 194 - Parametrizando Comportamentos pt 02

---
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest02.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.test;
+
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComportamentoPorParametroTest02 {
+    private static List<Car> cars = List.of(new Car("green",2011), new Car("black",1998), new Car("red",2019));
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/academy/devdojo/maratonajava/javacore/ZZBcomportamento/test/ComportamentoPorParametroTest03.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.ZZBcomportamento.test;
+
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.dominio.Car;
+import academy.devdojo.maratonajava.javacore.ZZBcomportamento.interfaces.CarPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class ComportamentoPorParametroTest03 {
+    private static List<Car> cars = List.of(new Car("green",2011), new Car("black",1998), new Car("red",2019));
+
+    public static void main(String[] args) {
+
+
+        List<Car> greenCars = filter(cars,(car) -> car.getColor().equalsIgnoreCase("green"));
+        List<Car> redCars = filter(cars,(car) -> car.getColor().equalsIgnoreCase("red"));
+        List<Car> yearBeforeCars = filter(cars,(car) -> car.getYear() < 2015);
+        System.out.println(redCars);
+        System.out.println(greenCars);
+        System.out.println(yearBeforeCars);
+
+    }
+
+    
+
+}
