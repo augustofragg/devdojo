84abc75cb0952e7536f1c1810315212bdbb487e7
Augusto fragg <augustofragg@gmail.com>
2025-07-29 17:19:13 -0300

    Aula 199 - Method Reference pt 02 - Referência a métodos não estáticos

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambda/test/MethodReferenceTest02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.ZZClambda.test;
+
+import academy.devdojo.maratonajava.javacore.ZZClambda.dominio.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambda.service.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodReferenceTest02 {
+    public static void main(String[] args) {
+        //Instanciando a classe AnimeComparators
+        //Para acessar os metodos não estáticos
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("One piece", 900), new Anime("Naruto", 500)));
+
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+
+        System.out.println(animeList);
+    }
+}
